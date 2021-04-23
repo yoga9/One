91caceae7fb32073eb37b8cea8f7a1e1a3787441
yoga9 <80407876+yoga9@users.noreply.github.com>
2021-04-23 08:58:43 +0530

    Add files via upload

---
+++ TestJava.java
@@ -0,0 +1,8 @@
+package packages1.packages2.packages3;   //we must use here, bcoz its a user defined package.
+
+public class TestJava {
+	public static void main(String[] args) {
+		System.out.println("Hello World -- This is Yogeshwari");
+		
+	}
+}
