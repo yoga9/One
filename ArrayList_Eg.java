1eca35dfe61517c359097ea614b8ee0418729302
yoga9 <80407876+yoga9@users.noreply.github.com>
2021-04-23 09:03:22 +0530

    Add files via upload

---
+++ ArrayList_Eg.java
@@ -0,0 +1,19 @@
+package dataStructure;           //ArrayList - 1
+
+import java.util.ArrayList;
+
+public class ArrayList_Eg {
+
+	public static void main(String[] args) {
+		ArrayList<String> obj = new ArrayList<String>();
+		
+		//adding elements to array list.
+		obj.add("Gayu");
+		obj.add("Leena");
+		obj.add("Pavi");
+		obj.add("Deelip");
+		
+		
+		
+	}
+}
