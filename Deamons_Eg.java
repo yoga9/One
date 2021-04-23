69b459fe9085a5e9685b42329aa3cd33d71d6211
yoga9 <80407876+yoga9@users.noreply.github.com>
2021-04-23 08:57:21 +0530

    Add files via upload

---
+++ Deamons_Eg.java
@@ -0,0 +1,10 @@
+package threads;   //Multithreading  --1
+/*if i run mutiple prgm all prgrm acquire the same m/y, 
+at a tym one prgrm is can excuted , rest of the prgrm wait in a queue., 
+supose if i dont use thread.sleep(4000) --> if i run thz all prgrm at a tym,
+prgm will throw some exception lyk m/y is not free or available.) */ 
+
+
+public class Deamons_Eg {
+
+}
